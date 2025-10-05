882e0f7e4159fe44e70ad7e18014a7584091d873
abdirahmanhassann <abdi12340.am@gmail.com>
2025-10-05 21:31:11 +0100

    resolved signup bugs

--- src/Routes/SignUp.jsx
+++ src/Routes/SignUp.jsx
@@ -1,9 +1,11 @@
 import { CircularProgress, Input } from '@mui/material';
+import { userInfo } from 'os';
 import React, { useEffect, useState } from 'react'
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 function SignUp() {
     let [signUpError,setSignUpError]= useState(false);
+    let [passwordError,setPasswordError]= useState(false);
     let [loading, setLoading]=useState(false)
     let navigate=useNavigate();
     let [signUp, setSignUp] =useState({
@@ -27,9 +29,15 @@ function SignUp() {
     }
     function submit(e) {
         e.preventDefault();
-                setLoading(true);
-             //   setTimeout(()=>{
-                    const requestOptions = {
+        console.log(signUp.password[0],signUp.rePassword[0])
+        setPasswordError(false)
+        if(signUp.password[0]!==signUp.rePassword[0]){
+            setPasswordError(true)
+        }
+        else{
+            setLoading(true);
+            //   setTimeout(()=>{
+                const requestOptions = {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ name:signUp.name[0], 
@@ -54,14 +62,15 @@ function SignUp() {
                         setLoading(false);
                     })
                     setLoading(false);
-    }
+                }
+            }
     return (
         <>
         <div className='main-top-div'>
             <h1>Welcome to the Todo List App</h1>
         <div className='main-centre-div'>
             <form onsubmit="return false;">
-                <h3>Login</h3>
+                <h3>Sign up</h3>
                 <Input color='secondary' required={true} className='input' placeholder='Username' name='username' onChange={(e) => handleChangeFunction(e)} />
                 <Input color='secondary' required={true} className='input' placeholder='Name' name='name' onChange={(e) => handleChangeFunction(e)} />
                 <Input color='primary' required={true} type='password' placeholder='Password' name='password' onChange={(e) => handleChangeFunction(e)} />
@@ -83,7 +92,11 @@ function SignUp() {
         </div>
         {
                         signUpError &&
-                        <p style={{color:'red'}}>Username or email already in use</p>
+                        <p style={{color:'red'}}>Username already in use</p>
+                    }
+        {
+                        passwordError &&
+                        <p style={{color:'red'}}>Passwords do not match</p>
                     }
         </div>
         </>
